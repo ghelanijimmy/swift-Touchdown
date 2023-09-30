a423bb762b53a5aa69d52a209ddf01d1666cead8
Jimmy Ghelani <ghelanijimmy@jgdesigns.ca>
2023-09-30 07:10:54 -0400

    Initial Commit

---
+++ Touchdown/TouchdownApp.swift
@@ -0,0 +1,17 @@
+//
+//  TouchdownApp.swift
+//  Touchdown
+//
+//  Created by Jimmy Ghelani on 2023-09-30.
+//
+
+import SwiftUI
+
+@main
+struct TouchdownApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
